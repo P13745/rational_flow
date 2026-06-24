d5eb15688fd78186d3d6751c7151d53de2bdf4d8
P13745 <tarosan_1e7b91@icloud.com>
2026-06-25 01:26:26 +0900

    Introduce module entry point

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const initialSeedDelay = 2;
+export const tableRenderInterval = 180;
+export const diesisCollectionCookie = "rf_diesis_seen";
+export const germanPitchClasses = ["C", "Cis/Des", "D", "Dis/Es", "E", "F", "Fis/Ges", "G", "Gis/As", "A", "B", "H"];
+export const maxSafeRatioInteger = Number.MAX_SAFE_INTEGER;
